d613e21c2d4aba914e63dfac75df42880b3652f4
Mauro Melo <mauromelo1@gmail.com>
2021-04-14 18:02:32 -0300

    Criando os componentes Display e Teclado

---
+++ src/br/com/mauromelo1/calc/visao/Botao.java
@@ -0,0 +1,8 @@
+package br.com.mauromelo1.calc.visao;
+
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton {
+
+}
---
+++ src/br/com/mauromelo1/calc/visao/Teclado.java
@@ -0,0 +1,14 @@
+package br.com.mauromelo1.calc.visao;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel {
+
+	public Teclado() {
+		setBackground(Color.RED);
+	}
+
+}
