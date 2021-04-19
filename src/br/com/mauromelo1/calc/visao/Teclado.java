47b61b07d719b6379298f5cf16fd80e14b14f330
Mauro Melo <mauromelo1@gmail.com>
2021-04-19 14:08:36 -0300

    Componente teclado #2

--- src/br/com/mauromelo1/calc/visao/Teclado.java
+++ src/br/com/mauromelo1/calc/visao/Teclado.java
@@ -22,11 +22,16 @@ public class Teclado extends JPanel {
 		GridBagConstraints c = new GridBagConstraints();
 
 		setLayout(layout);
+		
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
 
 		// Linha 1
+		
+		c.gridwidth = 3;
 		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
-		adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
-		adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+		c.gridwidth = 1;
 		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
 		// Linha 2
@@ -48,8 +53,9 @@ public class Teclado extends JPanel {
 		adicionarBotao("+", COR_LARANJA, c, 3, 3);
 
 		// Linha 5
+		c.gridwidth = 2;
 		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
-		adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+		c.gridwidth = 1;
 		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
 		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
