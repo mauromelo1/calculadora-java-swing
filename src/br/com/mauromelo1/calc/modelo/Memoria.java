150898a7f838679e84b58b314715e5cfb368a355
Mauro Melo <mauromelo1@gmail.com>
2021-04-19 18:33:39 -0300

    Logica da calculadora #5

--- src/br/com/mauromelo1/calc/modelo/Memoria.java
+++ src/br/com/mauromelo1/calc/modelo/Memoria.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memoria {
-	
+
 	private enum TipoComando {
 		ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
 	};
@@ -38,61 +38,77 @@ public class Memoria {
 	}
 
 	public void processarComando(String texto) {
-		
+
 		TipoComando tipoComando = detectarTipoComando(texto);
-		
+
 		if (tipoComando == null) {
-			return ;
-		}
-		else if (tipoComando == TipoComando.ZERAR) {
+			return;
+		} else if (tipoComando == TipoComando.ZERAR) {
 			textoAtual = "";
 			textoBuffer = "";
 			substituir = false;
 			ultimaOperacao = null;
-		}
-		else if (tipoComando == TipoComando.NUMERO 
-				|| tipoComando == TipoComando.VIRGULA) {
+		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
 			textoAtual = substituir ? texto : textoAtual + texto;
 			substituir = false;
+		} else {
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
 		}
-		
 
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
 	}
 
+	private String obterResultadoOperacao() {
+		if (ultimaOperacao == null) {
+			return textoAtual;
+		}
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+		double resultado = 0;
+		if (ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		} else if (ultimaOperacao == TipoComando.SUB) {
+			resultado = numeroBuffer - numeroAtual;
+		} else if (ultimaOperacao == TipoComando.MULT) {
+			resultado = numeroBuffer * numeroAtual;
+		} else if (ultimaOperacao == TipoComando.DIV) {
+			resultado = numeroBuffer / numeroAtual;
+		}
+		String texto = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = texto.endsWith(",0");
+
+		return inteiro ? texto.replace(",0", "") : texto;
+	}
+
 	private TipoComando detectarTipoComando(String texto) {
-		if(textoAtual.isEmpty() && texto == "0") {
+		if (textoAtual.isEmpty() && texto == "0") {
 			return null;
 		}
-		
+
 		try {
-		Integer.parseInt(texto);
-		return TipoComando.NUMERO;
-		}
-		catch(NumberFormatException e) {
+			Integer.parseInt(texto);
+			return TipoComando.NUMERO;
+		} catch (NumberFormatException e) {
 			// Quando não for número ...
 			if ("AC".equals(texto)) {
 				return TipoComando.ZERAR;
-			}
-			else if ("/".equals(texto)) {
+			} else if ("/".equals(texto)) {
 				return TipoComando.DIV;
-			}
-			else if ("*".equals(texto)) {
+			} else if ("*".equals(texto)) {
 				return TipoComando.MULT;
-			}
-			else if ("+".equals(texto)) {
+			} else if ("+".equals(texto)) {
 				return TipoComando.SOMA;
-			}
-			else if ("-".equals(texto)) {
+			} else if ("-".equals(texto)) {
 				return TipoComando.SUB;
-			}
-			else if ("=".equals(texto)) {
+			} else if ("=".equals(texto)) {
 				return TipoComando.IGUAL;
-			}
-			else if (",".equals(texto) && !textoAtual.contains(",")) {
+			} else if (",".equals(texto) && !textoAtual.contains(",")) {
 				return TipoComando.VIRGULA;
-			}			
-		}		
+			}
+		}
 		return null;
 	}
 
