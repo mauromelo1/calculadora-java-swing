c391413c68602efed31fef37b1bff1d44185326f
Mauro Melo <mauromelo1@gmail.com>
2021-04-19 15:29:39 -0300

    Logica da calculadora #1

---
+++ src/br/com/mauromelo1/calc/modelo/Memoria.java
@@ -0,0 +1,24 @@
+package br.com.mauromelo1.calc.modelo;
+
+public class Memoria {
+	
+	private static final Memoria instancia = new Memoria();
+	
+	private String textoAtual = "";
+	
+	private Memoria() {
+	}
+
+	public static Memoria getInstancia() {
+		return instancia;
+	}
+
+	public String getTextoAtual() {
+		return textoAtual.isEmpty()? "0": textoAtual;
+	}
+	
+	
+	
+	
+
+}
